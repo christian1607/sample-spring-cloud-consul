59b00a22b77d4f9aefc78a0da90972b511a89795
Christian Altamirano <christian.altamirano.ayala@gmail.com>
2020-05-09 22:28:42 -0500

    update version spring cloud

--- customer-service/src/main/java/pl/piomin/services/customer/CustomerApplication.java
+++ customer-service/src/main/java/pl/piomin/services/customer/CustomerApplication.java
@@ -6,7 +6,6 @@ import pl.piomin.services.customer.repository.CustomerRepository;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
